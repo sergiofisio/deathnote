92ca2f30685a69d480bea6ba2640febfa09c7909
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-04-01 18:37:33 -0300

    shinigamis

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-document.querySelector(".open").addEventListener("click", function (event) {
-  event.preventDefault();
-  document.body.classList.add("openBook");
-  setTimeout(() => {
-    window.location.href = this.href;
-  }, 2000);
+window.addEventListener("load", function () {
+  document.body.classList.add("fadein");
+});
+
+window.addEventListener("beforeunload", function () {
+  document.body.classList.remove("fadein");
 });
